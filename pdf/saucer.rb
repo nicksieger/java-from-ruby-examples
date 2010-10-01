2937575b5cf860e09a3d8d9bbb6ad96ffd0599d2
Nick Sieger <nick@nicksieger.com>
2010-10-01 14:08:36 -0400

    Add supporting jars for flyingsaucer

--- pdf/saucer.rb
+++ pdf/saucer.rb
@@ -1,10 +1,15 @@
 require 'java'
-require 'flying_saucer'
+HERE = File.dirname(__FILE__)
+puts "I'm here: #{HERE}"
+Dir["#{HERE}/*.jar"].each do |jar| 
+  puts "Requiring #{jar}"
+  require jar
+end
 
 java_import org.xhtmlrenderer.pdf.ITextRenderer
 
 document = <<-HTML
-<html><body><h1>Hello Flying Saucer!</h1></body></html>
+<html><body><h1>Hello JRubyConf!</h1></body></html>
 HTML
 
 File.open("doc.pdf", "wb") do |out|
